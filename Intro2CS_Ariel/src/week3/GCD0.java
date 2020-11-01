cd8a959141aa04368cc78ea497d26091147e4b80
boazbenmoshe <benmo@g.ariel.ac.il>
2020-11-01 08:44:07 +0200

    week3 GCD0-3 example

--- Intro2CS_Ariel/src/week3/GCD0.java
+++ Intro2CS_Ariel/src/week3/GCD0.java
@@ -4,20 +4,19 @@ package week3;
  * @author boaz.benmoshe
  *
  */
-public class GCD1 {
+public class GCD0 {
 
 	public static void main(String[] args) {
 		int x = 12;
 		int y = 20;
 		int t = 0;
-		int min = Math.min(x, y);
-		int i=min, gcd=1;
+		int i=1, gcd=1;
 		System.out.print("GCD("+x+","+y+") = ");
-		while(i>=1 && gcd==1) {
+		while(i<=y) {
 			if(x%i==0 && y%i==0) {
 				gcd=i;
 			}
-			i=i-1;
+			i=i+1;
 		}
 		System.out.println(gcd);
 	}

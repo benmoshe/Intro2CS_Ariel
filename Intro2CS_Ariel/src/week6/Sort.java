09252fe654f97535d4e19f570f8e3164ec2d038e
boazbenmoshe <benmo@g.ariel.ac.il>
2020-11-22 10:36:12 +0200

    Added a slow sorting

--- Intro2CS_Ariel/src/week6/Sort.java
+++ Intro2CS_Ariel/src/week6/Sort.java
@@ -74,20 +74,52 @@ public class Sort {
 	      }
 	   }
 	}
+	/**
+	 * This function uses a simple additional array to implement insertion sort 
+	 * @param arr
+	 */
+	public static void myInsertionSort(int[] arr) {
+		int size = arr.length;
+		int[] copy = new int[size];
+	    for (int  i=0 ; i < arr.length ; i++)	    {
+	    	int index = findMySortedIndex(arr,i); // compute how many entries are smaller than arr[i]
+	    	int sameIndx = findHowManyAreTheSame(arr,i);
+	    	copy[index+sameIndx] = arr[i];
+	    }
+	    for(int i=0;i<size;i=i+1) {
+	    	arr[i] = copy[i];
+	    }
+	}
+	// computes the proper index of ind in the array
+	public static int findMySortedIndex(int[] a, int ind) {
+		int ans = 0;
+		for(int i=0;i<a.length;i=i+1) {
+			if(a[i]<a[ind]) {ans = ans +1;}
+		}
+		return ans;
+	}
+	// computes the proper index of ind in the array
+		public static int findHowManyAreTheSame(int[] a, int ind) {
+			int ans = 0;
+			for(int i=0;i<ind;i=i+1) {
+				if(a[i]==a[ind]) {ans = ans +1;}
+			}
+			return ans;
+		}
 	
 	public static void main(String[] args) {
-		final int SIZE = 10000; // then change upto 100000
+		final int SIZE = 40000; // then change upto 100000
 		int[] arr1 = MyArrayLibrary.randomIntArray(SIZE, SIZE);
 		int[] arr2 = Arrays.copyOf(arr1,arr1.length);
 		int[] arr3 = Arrays.copyOf(arr1,arr1.length);
 		int[] arr4 = Arrays.copyOf(arr1,arr1.length);
-		//int[] arr5 = Arrays.copyOf(arr1,arr1.length);
+		int[] arr5 = Arrays.copyOf(arr1,arr1.length);
 
 		//////// bubbleSort
 		long start = System.currentTimeMillis();
 		bubbleSort(arr1);
 		long end = System.currentTimeMillis();
-		System.out.println("Bubble sort time = "+(end-start)/1000.+" secs,  is sorted? "+ MyArrayLibrary.isSortedAscending(arr1));
+		System.out.println("Bubble sort time = "+(end-start)/1000.0+" secs,  is sorted? "+ MyArrayLibrary.isSortedAscending(arr1));
 		
 		//////// selectionSort
 		start = System.currentTimeMillis();
@@ -101,12 +133,18 @@ public class Sort {
 		end = System.currentTimeMillis();
 		System.out.println("Insertion sort time = "+(end-start)/1000.+" secs,  is sorted? "+ MyArrayLibrary.isSortedAscending(arr3));
 
+		///////// MyInsertionSort (should be slow!!)
+		start = System.currentTimeMillis();
+		myInsertionSort(arr4);
+		end = System.currentTimeMillis();
+		System.out.println("MySlowInsertion sort time = "+(end-start)/1000.+" secs,  is sorted? "+ MyArrayLibrary.isSortedAscending(arr4));
+		
 		////////// Java  built-in sort (Dual-pivot Quicksort)
 		start = System.currentTimeMillis();
 		Arrays.sort(arr4);
 		end = System.currentTimeMillis();
 		System.out.println("Java sort time = "+(end-start)/1000.+" secs,  is sorted? "+ MyArrayLibrary.isSortedAscending(arr4));						
-		
+		/**
 		// now with a partially sorted array
 		System.out.println();
 		System.out.println("Now with a partially sorted array:");
@@ -146,6 +184,7 @@ public class Sort {
 			System.out.println("Found at index " + found + " the item " + arr4[found]);
 		else
 			System.out.println("I didn't find it");*/
+		
 	}
 	
 	
